b7bfedab779ecd779c09bf4c9ba4d8c8fad4ab00
Giorgio Silvestris <giorgio.silvestris@gmail.com>
2021-11-19 11:08:12 +0100

    update

--- gui/run-CMS-gui.cmd
+++ gui/run-CMS-gui.cmd
@@ -1,1 +1,15 @@
-"%JAVA_HOME%"\bin\java -cp target/CMS-gui-1.0.0.jar;lib/swing3rd.jar;lib/swingup-1.0.0.jar org.dew.swingup.main.Main
+@echo off
+
+if "%JAVA_HOME%"=="" goto NOJAVAHOME
+
+set _CP=target/CMS-gui-1.0.0.jar;lib/swing3rd.jar;lib/swingup-1.0.0.jar
+
+"%JAVA_HOME%"\bin\java -cp %_CP% org.dew.swingup.main.Main
+
+goto DONE
+
+:NOJAVAHOME
+echo JAVA_HOME not setted.
+goto DONE
+
+:DONE
--- gui/run-CMS-gui.sh
+++ gui/run-CMS-gui.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-"$JAVA_HOME"/bin/java -cp ./target/CMS-gui-1.0.0.jar:./target/swingup-1.0.0.jar:./lib/swing3rd.jar org.dew.swingup.main.Main
+CP="./target/CMS-gui-1.0.0.jar:./target/swingup-1.0.0.jar:./lib/swing3rd.jar"
+
+if [ "x$JAVA_HOME" = "x" ]; then
+  echo "JAVA_HOME not setted"
+else
+  "$JAVA_HOME"/bin/java -cp "$CP" org.dew.swingup.main.Main
+fi
